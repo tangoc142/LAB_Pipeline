b912e53d109116b4afa67fca1ecf83500d145d61
imran-nk <mohamedimr@gmail.com>
2022-07-17 19:23:32 +0530

    Added checkip file

---
+++ checkip.py
@@ -0,0 +1,17 @@
+import os
+
+from netmiko import ConnectHandler
+
+user = os.environ["USER"]
+password = os.environ["PASSWORD"]
+
+iosv_l2_s1 = {
+    "device_type": "cisco_ios",
+    "ip": "192.168.0.108",
+    "username": user,
+    "password": password,
+}
+
+net_connect = ConnectHandler(**iosv_l2_s1)
+output = net_connect.send_command("show ip int brief")
+print(output)
